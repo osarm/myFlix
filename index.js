aff2ef9ea0ba2e132692193d5329842b3e01eafb
osarm <161492930+osarm@users.noreply.github.com>
2024-09-07 17:57:13 -0400

    Connection URI
    If applied, this commit will prevent my connection URI from being exposed

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
 
-mongoose.connect('mongodb://localhost:27017/TestFlixDB')
+// mongoose.connect('mongodb://localhost:27017/TestFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB...', err));
 
