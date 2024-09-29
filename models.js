a4b2d4fde92c6f0ffdf137c3082dddd9445b1253
osarm <161492930+osarm@users.noreply.github.com>
2024-09-28 21:25:53 -0400

    Update models.js
    If applied, this commit will update the userSchema and add some descriptions

--- models.js
+++ models.js
@@ -18,25 +18,25 @@ let movieSchema = mongoose.Schema({
   Featured: Boolean
 });
 
-let userSchema = mongoose.Schema({
-  name: {type: String},
-  Username: {type: String, required: true},
-  Password: {type: String, required: true},
-  Email: {type: String, required: true},
+const userSchema = mongoose.Schema({
+  Username: { type: String, required: true, unique: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true, unique: true },
   Birthday: Date,
-  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
+//method to hash password
 userSchema.statics.hashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
 };
-
+// method to validate password
 userSchema.methods.validatePassword = function(password) {
   return bcrypt.compareSync(password, this.Password);
 };
 
 let Movie = mongoose.model('Movie', movieSchema);
-let User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
